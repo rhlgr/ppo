ed2938b55c7ee5f8f98404934a1ae259f69a3277
rhlgr1 <rg512g1@gmail.com>
2023-02-25 11:18:40 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#link").click(function (e) {
+        e.preventDefault();
+          
+        window.location.href 
+            = "File/randomfile.docx";
+    });
+});
